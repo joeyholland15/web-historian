17e16048eaf2e02bf15f527744b90f419e4baab6
joeyholland15 <joeyholland15@gmail.com>
2015-12-16 12:34:48 -0800

    Started out setting up get and post conditionals and set up data/end listeners

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,25 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-// require more modules/folders here!
+var headers = require('./http-helpers');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.method === "GET") {
+
+    res.writeHead(200, headers.headers);
+    res.end(archive.paths.list);
+  } else if (req.method === "POST") {
+    var data = '';
+
+    req.on('data', function(chunk) {
+      data += chunk;
+    });
+
+    req.on('end', function() {
+      // take our data and put it somewhere
+      // interact with archive helpers
+    });
+
+    res.writeHead(201, headers.headers);
+    res.end(archive.paths.list);
+  }
 };
